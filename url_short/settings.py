e39a41c482327b79f06adf6e82ca5fedd1a05d56
Dinesh <182dns@gmail.com>
2022-05-01 20:06:12 +0545

    Production tested

--- url_short/settings.py
+++ url_short/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://urlshortner-182.herokuapp.com/' , '127.0.0.1:8000']
 
 
 # Application definition
